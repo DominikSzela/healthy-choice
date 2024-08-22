4295c0d305d3059cacca472629920a80fa262787
Dominik <spiderqwerty@gmail.com>
2024-08-22 20:56:11 +0200

    improvement Diet component

--- src/components/Diet/Diet.jsx
+++ src/components/Diet/Diet.jsx
@@ -12,21 +12,47 @@ const Diet = () => {
         >
             {diet && (
                 <div className={styles.dietBox}>
-                    <h1>Dieta {diet.name}</h1>
-                    <p className={styles.description}>Podczas diety, jak i na co dzień, bardzo ważne jest spożywanie produktów dobrej jakości. Ważne jest również dbanie o zdrowe nawyki, takie jak odpowiednia długość i jakość snu, picie odpowiedniej ilości wody oraz regularny odpoczynek.</p>
-                    <h2>Twoje zalecane proporcje to:</h2>
-                    <div className={styles.macronutrients}>
-                        <div className={styles.proportion}>
-                            <div className={styles.type}>Białko:</div>
-                            <div className={styles.value}>{diet.protein}%</div>
+                    <h1 className={styles.title}>Dieta {diet.name}</h1>
+                    <p className={styles.generalDescription}>Podczas diety, jak i na co dzień, bardzo ważne jest spożywanie produktów dobrej jakości. Ważne jest również dbanie o zdrowe nawyki, takie jak odpowiednia długość i jakość snu, picie odpowiedniej ilości wody oraz regularny odpoczynek.</p>
+                    <h2>Twoja przemiana materii:</h2>
+                    <div className={styles.details}>
+                        <div className={styles.ratio}>
+                            <div>Podstawowa (PPM):</div>
+                            <div>{diet.PPM} kcal</div>
                         </div>
-                        <div className={styles.proportion}>
-                            <div className={styles.type}>Węglowodany:</div>
-                            <div className={styles.value}>{diet.carbohydrates}%</div>
+                        <div className={styles.ratio}>
+                            <div>Całkowita (CPM):</div>
+                            <div>{diet.CPM} kcal</div>
                         </div>
-                        <div className={styles.proportion}>
-                            <div className={styles.type}>Tłuszcze:</div>
-                            <div className={styles.value}>{diet.fat}%</div>
+                    </div>
+                    <h2>Twoje zalecane proporcje procentowe:</h2>
+                    <div className={styles.details}>
+                        <div className={styles.ratio}>
+                            <div>Białko:</div>
+                            <div>{diet.protein} %</div>
+                        </div>
+                        <div className={styles.ratio}>
+                            <div>Węglowodany:</div>
+                            <div>{diet.carbohydrates} %</div>
+                        </div>
+                        <div className={styles.ratio}>
+                            <div>Tłuszcze:</div>
+                            <div>{diet.fat} %</div>
+                        </div>
+                    </div>
+                    <h2>Twoje zalecane proporcje w kcal:</h2>
+                    <div className={styles.details}>
+                        <div className={styles.ratio}>
+                            <div>Białko:</div>
+                            <div>{diet.protein * diet.CPM / 100} kcal</div>
+                        </div>
+                        <div className={styles.ratio}>
+                            <div>Węglowodany:</div>
+                            <div>{diet.carbohydrates * diet.CPM / 100} kcal</div>
+                        </div>
+                        <div className={styles.ratio}>
+                            <div>Tłuszcze:</div>
+                            <div>{diet.fat * diet.CPM / 100} kcal</div>
                         </div>
                     </div>
                 </div >
